b6a7834885d42e63994ce446066f558925b519ad
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2018-04-05 16:29:13 +0800

    Lowercase text before looking up labels in body and title

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = (robot) => {
     labels.data.map(label => labelList.push(label.name))
     labelList
       .filter(label => !config.excludeLabels.includes(label))
-      .map(label => issue.data.title.includes(label) || issue.data.body.includes(label) ? labelsToAdd.push(label) : null)
+      .map(label => issue.data.title.toLowerCase().includes(label) || issue.data.body.toLowerCase().includes(label) ? labelsToAdd.push(label) : null)
 
     return context.github.issues.addLabels(context.issue({ labels: labelsToAdd }))
   })
