0517d46886250f27082513219d6d3e7d9854af8f
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2018-04-05 15:26:40 +0800

    Bot logic

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = (robot) => {
+  robot.on('issues.opened', async context => {
+    const config = await context.config('labeler.yml', { numLabels: 20 })
+    const labels = await context.github.issues.getLabels(context.issue({ per_page: config.numLabels }))
+    const issue = await context.github.issues.get(context.issue())
+
+    let labelList = []
+    let labelsToAdd = []
+
+    labels.data.map(label => labelList.push(label.name))
+    labelList
+      .filter(label => !config.excludeLabels.includes(label))
+      .map(label => issue.data.title.includes(label) || issue.data.body.includes(label) ? labelsToAdd.push(label) : null)
+
+    return context.github.issues.addLabels(context.issue({ labels: labelsToAdd }))
+  })
+}
