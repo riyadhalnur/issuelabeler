6e4a76d7a137cc21f90cc700a0d8c4b25aa40f98
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2019-07-20 12:33:43 +0800

    Bug: Fix calls to github

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ const fs = require('fs');
 
 module.exports = robot => {
   robot.on('issues.opened', async context => {
+    const issueCtx = context.issue();
+
     const config = await context.config('labeler.yml', { numLabels: 20 });
-    const labels = await context.github.issues.getLabels(context.issue({ per_page: config.numLabels }));
-    const issue = await context.github.issues.get(context.issue());
+    const labels = await context.github.issues.listLabelsForRepo(context.issue({ per_page: config.numLabels }));
+    const issue = await context.github.issues.get(context.issue({ issue_number: context.payload.issue.number }));
 
     let labelList = [];
     let labelsToAdd = [];
@@ -16,6 +18,6 @@ module.exports = robot => {
       .filter(label => !config.excludeLabels.includes(label))
       .map(label => issue.data.title.toLowerCase().includes(label) || issue.data.body.toLowerCase().includes(label) ? labelsToAdd.push(label) : null);
 
-    return context.github.issues.addLabels(context.issue({ labels: labelsToAdd }));
+    return context.github.issues.addLabels(context.issue({ issue_number: issueCtx.number, labels: labelsToAdd }));
   });
 };
