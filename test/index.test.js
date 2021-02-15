27ac396e8d76599e0b365a623f309955f5b02f3b
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2021-02-15 21:26:05 +0800

    UPDATED: unit tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,100 +1,113 @@
-const { serverless } = require('@probot/serverless-lambda');
+const {
+  Probot,
+  ProbotOctokit,
+} = require("@probot/adapter-aws-lambda-serverless");
 
-const handler = require('../src/handler');
-const app = require('../src/app');
+const nock = require("nock");
+nock.disableNetConnect();
 
-const event_opened = require('./fixtures/event_opened');
-const event_edited = require('./fixtures/event_edited');
-const payload = require('./fixtures/payload');
+const handler = require("../src/handler");
+const app = require("../src/app");
 
-describe('issuelabeler', () => {
-  let context
-  let github
+const event_opened = require("./fixtures/event_opened");
+const event_edited = require("./fixtures/event_edited");
+const payload = require("./fixtures/payload");
+
+describe("issuelabeler", () => {
+  let probot;
 
   beforeAll(() => {
-    github = {
-      repos: {
-        getContents: jest.fn().mockReturnValue(Promise.resolve({
-          data: {
-            content: Buffer.from(`excludeLabels:\n  - hey`).toString('base64')
-          }
-        }))
-      },
-      issues: {
-        listLabelsForRepo: jest.fn().mockReturnValue(Promise.resolve({
-          data: [
-            {
-              id: 889466157,
-              url: 'https://api.github.com/repos/issuebot/test/labels/hey',
-              name: 'hey',
-              color: 'f5fc92',
-              default: false
-            },
-            {
-              id: 889466158,
-              url: 'https://api.github.com/repos/issuebot/test/labels/test',
-              name: 'test',
-              color: 'f5fc93',
-              default: false
-            },
-            {
-              id: 889466159,
-              url: 'https://api.github.com/repos/issuebot/test/labels/a-feature',
-              name: 'A-Feature',
-              color: 'f5fc93',
-              default: false
-            }
-          ]
-        })),
-        get: jest.fn().mockReturnValue(Promise.resolve({
-          data: payload
-        })),
-        addLabels: jest.fn().mockReturnValue(Promise.resolve({}))
-      }
-    };
-
-    context = { done: jest.fn() };
-    handler.bot = serverless(async robot => {
-      robot.auth = jest.fn().mockResolvedValue(github)
-      robot.on(handler.events, app)
+    probot = new Probot({
+      // simple authentication as alternative to appId/privateKey
+      githubToken: "test",
+      // disable logs
+      logLevel: "warn",
+      // disable request throttling and retries
+      Octokit: ProbotOctokit.defaults({
+        throttle: { enabled: false },
+        retry: { enabled: false },
+      }),
     });
+    probot.load(app);
   });
 
-  test('that labels are applied to an issue when opened', async () => {
-    await handler.bot(event_opened, context);
+  test("that labels are applied to an issue when opened", async () => {
+    const mock = nock("https://api.github.com")
+      .get("/repos/issuebot/test/contents/.github%2Flabeler.yml")
+      .reply(200, Buffer.from(`excludeLabels:\n  - hey`).toString())
+      .get("/repos/issuebot/test/labels?issue_number=2&per_page=100")
+      .reply(200, [
+        {
+          id: 889466157,
+          url: "https://api.octokit.com/repos/issuebot/test/labels/hey",
+          name: "hey",
+          color: "f5fc92",
+          default: false,
+        },
+        {
+          id: 889466158,
+          url: "https://api.octokit.com/repos/issuebot/test/labels/test",
+          name: "test",
+          color: "f5fc93",
+          default: false,
+        },
+        {
+          id: 889466159,
+          url: "https://api.octokit.com/repos/issuebot/test/labels/a-feature",
+          name: "A-Feature",
+          color: "f5fc93",
+          default: false,
+        },
+      ])
+      .get("/repos/issuebot/test/issues/2")
+      .reply(200, payload)
+      .post("/repos/issuebot/test/issues/2/labels", (requestBody) => {
+        expect(requestBody).toEqual(["test", "A-Feature"]);
+        return true;
+      })
+      .reply(200, []);
 
-    expect(github.repos.getContents).toHaveBeenCalled();
-    expect(github.issues.listLabelsForRepo).toHaveBeenCalled();
-    expect(github.issues.get).toHaveBeenCalled();
-    expect(github.issues.addLabels).toHaveBeenCalledWith({
-      issue_number: 2,
-      number: 2,
-      owner: 'issuebot',
-      repo: 'test',
-      labels: [
-        'test',
-        'A-Feature'
-      ]
-    });
-    expect(context.done).toHaveBeenCalled();
+    await probot.receive(event_opened);
+    expect(mock.activeMocks()).toEqual([]);
   });
 
-  test('that labels are applied to an issue when edited', async () => {
-    await handler.bot(event_edited, context);
+  test("that labels are applied to an issue when edited", async () => {
+    const mock = nock("https://api.github.com")
+      .get("/repos/issuebot/test/contents/.github%2Flabeler.yml")
+      .reply(200, Buffer.from(`excludeLabels:\n  - hey`).toString())
+      .get("/repos/issuebot/test/labels?issue_number=2&per_page=100")
+      .reply(200, [
+        {
+          id: 889466157,
+          url: "https://api.octokit.com/repos/issuebot/test/labels/hey",
+          name: "hey",
+          color: "f5fc92",
+          default: false,
+        },
+        {
+          id: 889466158,
+          url: "https://api.octokit.com/repos/issuebot/test/labels/test",
+          name: "test",
+          color: "f5fc93",
+          default: false,
+        },
+        {
+          id: 889466159,
+          url: "https://api.octokit.com/repos/issuebot/test/labels/a-feature",
+          name: "A-Feature",
+          color: "f5fc93",
+          default: false,
+        },
+      ])
+      .get("/repos/issuebot/test/issues/2")
+      .reply(200, payload)
+      .post("/repos/issuebot/test/issues/2/labels", (requestBody) => {
+        expect(requestBody).toEqual(["test", "A-Feature"]);
+        return true;
+      })
+      .reply(200, []);
 
-    expect(github.repos.getContents).toHaveBeenCalled();
-    expect(github.issues.listLabelsForRepo).toHaveBeenCalled();
-    expect(github.issues.get).toHaveBeenCalled();
-    expect(github.issues.addLabels).toHaveBeenCalledWith({
-      issue_number: 2,
-      number: 2,
-      owner: 'issuebot',
-      repo: 'test',
-      labels: [
-        'test',
-        'A-Feature'
-      ]
-    });
-    expect(context.done).toHaveBeenCalled();
+    await probot.receive(event_edited);
+    expect(mock.activeMocks()).toEqual([]);
   });
 });
