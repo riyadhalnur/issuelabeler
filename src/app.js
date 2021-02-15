7ae9541285132efc6c23ca6bb23228e6c632402b
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2021-02-15 23:27:48 +0800

    ADDED: explicit labeling
    - add support for overriding default bot behaviour
    - if supplied with custom array in config file, it'll
    take precedence over usual labelling logic

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
-"use strict";
+'use strict';
 
-const eventTypes = ["issues.opened", "issues.edited"];
+const eventTypes = ['issues.opened', 'issues.edited'];
+const loc_body = 'body';
+const loc_title = 'title';
+
+const addCustomLabels = (issue, config) => {
+  let labels = [];
+
+  config.map((custCfg) => {
+    if (
+      custCfg.location === loc_body &&
+      issue.body.toLowerCase().includes(custCfg.keywords.join(','))
+    ) {
+      labels.push(...custCfg.labels);
+    }
+
+    if (
+      custCfg.location === loc_title &&
+      issue.title.toLowerCase().includes(custCfg.keywords.join(','))
+    ) {
+      labels.push(...custCfg.labels);
+    }
+  });
+
+  return labels;
+};
 
 module.exports = async (app) => {
   app.on(eventTypes, async (context) => {
-    const config = await context.config("labeler.yml", { numLabels: 100 });
+    const config = await context.config('labeler.yml', {
+      numLabels: 100,
+      excludeLabels: [],
+    });
     const labels = await context.octokit.issues.listLabelsForRepo(
       context.issue({ per_page: config.numLabels })
     );
@@ -15,15 +42,19 @@ module.exports = async (app) => {
     let labelList = [];
     let labelsToAdd = [];
 
-    labels.data.map((label) => labelList.push(label.name));
-    labelList
-      .filter((label) => !config.excludeLabels.includes(label))
-      .map((label) =>
-        issue.data.title.toLowerCase().includes(label.toLowerCase()) ||
-        issue.data.body.toLowerCase().includes(label.toLowerCase())
-          ? labelsToAdd.push(label)
-          : null
-      );
+    if (config.custom) {
+      labelsToAdd = addCustomLabels(issue.data, config.custom);
+    } else {
+      labels.data.map((label) => labelList.push(label.name));
+      labelList
+        .filter((label) => !config.excludeLabels.includes(label))
+        .map((label) =>
+          issue.data.title.toLowerCase().includes(label.toLowerCase()) ||
+          issue.data.body.toLowerCase().includes(label.toLowerCase())
+            ? labelsToAdd.push(label)
+            : null
+        );
+    }
 
     return context.octokit.issues.addLabels(
       context.issue({
